8183f1f2b15d290770fc36398879bdc44c47458c
odunayo12 <odun.rotimi@gmail.com>
2020-11-09 09:42:27 +0100

    factor german cities

--- asset_script/scrapeWiki.py
+++ asset_script/scrapeWiki.py
@@ -15,7 +15,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 # %%
-startTime = datetime.now()
+#startTime = datetime.now()
 
 
 def make_soup(url):
@@ -29,26 +29,25 @@ make_soup_bowl = make_soup(
     "https://en.wikipedia.org/wiki/List_of_cities_and_towns_in_Germany")
 
 # %%
-for made_soup in make_soup_bowl.find("div", {"class": "mw-parser-output"}).finAll("table"):
-    for every_recipe in made_soup.finAll("li"):
-        print(every_recipe)
-# %%
-for each_li in make_soup_bowl:
-    state_text = each_li.text
-    print(state_text)
-
-# %%
 recipee_mall = make_soup_bowl.find("div", {"class": "mw-parser-output"})
 
 # %%
 recipee_table = recipee_mall.findAll('table')[0:25]
 # %%
 tasty_dish = []
+csv_data = {}
+item_no = 0
 for recipee_list in recipee_table:
     for spice_list in recipee_list.findAll("li"):
-        tasty_soup = spice_list.text
+        german_cities = tasty_soup = spice_list.text
         tasty_dish.append(tasty_soup)
+        item_no += 1
+        csv_data[item_no] = german_cities
+# print(csv_data)
+# %%
+german_cities = pd.DataFrame.from_dict(csv_data, orient='index',
+                                       columns=["german_cities"])
+german_cities.to_csv(r"../data/german_towns.csv")
+#print(datetime.now() - startTime)
 
 # %%
-pd.DataFrame(tasty_dish).to_csv(r"../data/german_towns.csv")
-print(datetime.now() - startTime)
