72f05ef15a6842b364de7f1a56688e08174a6786
odunayo12 <odun.rotimi@gmail.com>
2020-11-20 22:39:33 +0100

    concat merged bach and masters csv

--- asset_script/clean_german_towns.py
+++ asset_script/clean_german_towns.py
@@ -2,8 +2,10 @@
 # pyright: reportUnboundVariable=false
 import numpy as np
 from numpy.core.numeric import outer
+from numpy.lib.function_base import select
 import pandas as pd
 import re
+import glob
 
 from pandas.core.reshape.merge import merge
 
@@ -34,6 +36,8 @@ def clean_german_cities():
 def read_data_n_merge(data_file_address, clean_germancities):
     """
     read and merge csv
+    data_file_address = directory of the to-be read file
+    clean_germancities = aclean file with non-changing content to be merged. 
     """
     filename_ = data_file_address.rsplit("_", 2)[0].rsplit("/", 1)[1]
     data_address = pd.read_csv(data_file_address)
@@ -51,6 +55,17 @@ def read_data_n_merge(data_file_address, clean_germancities):
         german_states__nan = merge_out_name[merge_out_name["german_states"].isnull(
         )]
         print(german_states__nan.german_cities_x.unique())
+
+
+def summary_shape(df_1, df_2, df_1_n_2):
+    """
+    df_1, df_2 = dfs to merge 
+    df_1_n_2 after merge
+    the fxtn relates the shape of conctenated or merged dataframe before and after merge,
+    """
+    print_text = print(
+        f"The fisrt dataframe has {df_1.shape}\nThe Second dataframe has {df_2.shape}\nThe merged dataframe has {df_1_n_2.shape}")
+    return print_text
 
 
 # %%
@@ -99,3 +114,35 @@ print("There are {} missing values in merged_master_df".format(
 # %%
 german_states__nan = merged_bachelors_df[merged_bachelors_df["german_states"].isnull(
 )]
+
+# %%
+# concatenating merged files
+# stores file address with pattern = "merged"
+files_dir = glob.glob(r"../data/*merged*")
+len(files_dir)
+# %%
+bach_to_merge = pd.read_csv(files_dir[0])
+# %%
+dash_, slash_ = "_", "\\"
+# reads the stored file address in to csv
+all_df = [pd.read_csv(file_) for file_ in files_dir]
+
+# a dict returning each filename and colum names
+all_to_merge = [{f"{file_.rsplit(slash_, 1)[1]}": pd.read_csv(
+    file_).columns} for file_ in files_dir]
+all_to_merge
+all_df[0]
+# %%
+# select needed columns for masters and bachelors dataframe
+bach_n_master = [a[['course_name', 'uni_name',
+                    'german_cities_x', 'tution_fee', 'german_cities_dup',
+                    'german_cities_y', 'german_states',  'lang_of_instr',
+                    'dura_of_study', 'semester_beginning', 'online_course_elemen']] for a in [all_df[0], all_df[2]]]
+# bach_n_master[0].head()
+bach_n_master_concatD = pd.concat([bach_n_master[0], bach_n_master[1]], axis=0)
+
+# %%
+#summary_shape(bach_n_master[0], bach_n_master[1], bach_n_master_concatD)
+bach_n_master_concatD.to_csv("../data/bach_n_master_concatD.csv")
+# %%
+# TODO: refactor columns content to binary or tenary
